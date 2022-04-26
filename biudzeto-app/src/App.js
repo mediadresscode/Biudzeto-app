3030fe9c2ffc90d90aefac16074e645271d6d9eb
MariusZintikas <98409697+MariusZintikas@users.noreply.github.com>
2022-04-26 17:53:27 +0300

    route

--- biudzeto-app/src/App.js
+++ biudzeto-app/src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import EarnsForm from "./components/EarnsForm";
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 
 function App() {
   return (
-    <>
-   
+    <Router>
     <div className="App">
+    <Switch>
       <header className="App-header">
        <Header/>
       </header>
@@ -16,11 +17,12 @@ function App() {
       <h1>Pajamos</h1>
         <EarnsForm/>
       </div>
+      </Switch>
       <footer className="App-footer">
       <Footer/>
       </footer>
     </div>
-    </>
+    </Router>
   );
 }
 
