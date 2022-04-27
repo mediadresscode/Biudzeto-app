45597ac043ecb269fc9295715455ae41c88903ac
jgudone <95044532+jgudone@users.noreply.github.com>
2022-04-27 12:25:07 +0300

    route

--- biudzeto-app/src/App.js
+++ biudzeto-app/src/App.js
@@ -1,34 +1,40 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import EarnsForm from "./components/EarnsForm";
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import IntroPage from './components/IntroPage';
-import HomePage from './components/HomePage';
-import LoginForm from './components/LoginForm';
-import RegistrationForm from './components/RegistrationForm';
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import IntroPage from "./components/IntroPage";
+import HomePage from "./components/HomePage";
+import LoginForm from "./components/LoginForm";
+import RegistrationForm from "./components/RegistrationForm";
+import CostsForm from "./components/CostsForm";
 
 function App() {
   return (
     <Router>
-    <div className="App">
-    <Switch>
-     <Route exact path="/">
-       <IntroPage/>
-     </Route>
-     <Route path="/HomePage">
-      <HomePage/>
-     </Route>
-     <Route path="/LoginForm">
-      <LoginForm/>
-     </Route>
-     <Route path="/RegistrationForm">
-      <RegistrationForm/>
-     </Route>
-     </Switch>
-    </div>
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            <IntroPage />
+          </Route>
+          <Route path="/HomePage">
+            <HomePage />
+          </Route>
+          <Route path="/LoginForm">
+            <LoginForm />
+          </Route>
+          <Route path="/RegistrationForm">
+            <RegistrationForm />
+          </Route>
+          <Route exact path="/EarnsForm.js">
+            <div className="page">
+              <EarnsForm />
+              <CostsForm />
+            </div>
+          </Route>
+        </Switch>
+      </div>
     </Router>
   );
 }
